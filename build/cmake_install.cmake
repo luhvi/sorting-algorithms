a5c3244972a4e458a06e2c069e94d5e5914b88ea
luhvi <luhvilive@gmail.com>
2025-12-08 14:37:25 +0100

    added boiler plate to make sorting algorithm visuals

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /Users/tore/Documents/projects/sfml-game
+# Install script for directory: C:/projects/sfml-game
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/sfml_game")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,13 +34,13 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "C:/mingw64/bin/objdump.exe")
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/Users/tore/Documents/projects/sfml-game/build/install_local_manifest.txt"
+  file(WRITE "C:/projects/sfml-game/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -56,6 +56,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/Users/tore/Documents/projects/sfml-game/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/projects/sfml-game/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- main.cpp
+++ main.cpp
@@ -6,29 +6,35 @@
 #include <SFML/Network.hpp>
 
 int main() {
-  sf::RenderWindow window(sf::VideoMode({640, 480}), "SFML Game");
-  
-  while(window.isOpen())
+  sf::RenderWindow window(sf::VideoMode({640, 480}), "Sorting Algorithms");
+
+  int dataSize { 50 };
+  sf::RectangleShape cols[dataSize];
+
+  for (int i = 0; i < dataSize; i++)
   {
-    while(auto event = window.pollEvent())
-    {
-      if (event->is<sf::Event::Closed>())
-      {
-        window.close();
-      }
-      
-      if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
-      {
-        if (keyPressed->code == sf::Keyboard::Key::Escape)
-        {
-          window.close();
-        }
-      }
-    }
-    
-    window.clear();
-    window.display();
+    float height = (i + 1) * 8.f;
+    cols[i].setSize(sf::Vector2f(10.f, height));
+    cols[i].setFillColor(sf::Color::Green);
+    cols[i].setPosition(sf::Vector2f(10.f + (i * 12), 480.f - height));
   }
-  
+
+  while (window.isOpen())
+  {
+    while (auto ev = window.pollEvent())
+    {
+      if (ev->is<sf::Event::Closed>())
+        window.close();
+    }
+    window.clear(sf::Color::Black);
+    
+    for (int i = 0; i < dataSize; i++)
+    {
+      window.draw(cols[i]);
+    }
+
+window.display();
+  }
+ 
   return 0;
 }
